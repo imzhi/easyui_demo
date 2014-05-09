f2cf9d47fbb4d54f9a1bb732ffa545ae56addc86
imzhi <yxz_blue@126.com>
2014-05-09 23:30:03 +0800

    修改

--- imzhi.me/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('APP_NAME', 'imzhi.me_App');
-define('APP_PATH', dirname(__FILE__).'/../'.APP_NAME.'/');
-
-define('APP_DEBUG', TRUE);
-
-require dirname(__FILE__).'/../ThinkPHP/ThinkPHP.php';
--- imzhi.me_App/Conf/debug_home.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return array(
-    'SHOW_PAGE_TRACE' => TRUE,
-
-    'DB_USER' => 'root',
-    'DB_PWD' => '1234',
-    'DB_DSN' => 'mysql:host=127.0.0.1;dbname=mybbs;charset=utf8',
-);
