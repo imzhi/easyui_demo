ab6c40fc98533d64d919f11d8aec3645d2b956e3
imzhi <yxz_blue@126.com>
2014-06-20 09:29:15 +0800

    修改

--- IMZHIME_APP/Lib/Action/Admin/PublicAction.class.php
+++ IMZHIME_APP/Lib/Action/Admin/PublicAction.class.php
@@ -1,23 +1,8 @@
 <?php
-class PublicAction extends Action {
-    public function _initialize() {
-    }
-
-    public function login() {
-        $this->display();
-    }
-
+class PublicAction extends CommonAction {
     public function vcode() {
         import('ORG.Util.Image');
         Image::buildImageVerify(4, 1, 'png', 48, 22, 'vcode');
-    }
-
-    public function test() {
-        $this->display();
-    }
-
-    public function edit() {
-        $this->display();
     }
 
     public function sign_up() {
